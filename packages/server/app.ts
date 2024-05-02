d762365c24bc0fbdae4c3f6dde8ae8c47d71c5b6
Jace Babin <jbabin91@gmail.com>
2024-05-01 17:14:22 -0700

    feat: add docker
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import { cors } from 'hono/cors';
+import { serveStatic } from 'hono/bun';
 import { logger } from 'hono/logger';
 
 import { expensesRoute } from './routes/expenses';
@@ -7,12 +7,10 @@ import { expensesRoute } from './routes/expenses';
 const app = new Hono();
 
 app.use(logger());
-app.use(cors());
-
-app.get('/', (c) => {
-  return c.json({ message: 'Hello, World!!' });
-});
 
 app.route('/api/expenses', expensesRoute);
+
+app.get('*', serveStatic({ root: '../frontend/dist' }));
+app.get('*', serveStatic({ path: '../frontend/dist/index.html' }));
 
 export default app;
